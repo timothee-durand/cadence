72c15bf0e37be63e1c615b011273f2863882ed91
Jessy <ernatusjessy@gmail.com>
2023-11-17 14:49:43 +0100

    fix shadcn finish init

--- src/playground/src/App.tsx
+++ src/playground/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
-      <h1>Connard</h1>
+      <Button>Click me</Button>
     </>
   )
 }

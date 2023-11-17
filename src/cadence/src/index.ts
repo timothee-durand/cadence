52e65cf5e3e9dbc15bad9b980839722b9d6ff5d6
Jessy <ernatusjessy@gmail.com>
2023-11-17 11:26:42 +0100

    change vue to react

--- src/cadence/src/index.ts
+++ src/cadence/src/index.ts
@@ -1,3 +1,11 @@
 export function helloWorld() {
   console.log('Hello World')
 }
+
+
+export function playSong() {
+  // Créer un nouvel objet Audio
+  var audio = new Audio("../Samples/808Bass/B1S.mp3");
+  // Jouer le son
+  audio.play();
+}

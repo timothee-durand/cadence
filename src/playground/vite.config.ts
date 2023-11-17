8284e84290c9bca874f2afc471c9a0786baea18c
mouzone9 <74732988+mouzone9@users.noreply.github.com>
2023-11-17 11:27:30 +0100

    change vue to react (#13)

--- src/playground/vite.config.ts
+++ src/playground/vite.config.ts
@@ -1,17 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: process.env.NODE_ENV === "production" ? "/iim-pfe-23-24/" : "/",
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [react()],
 })

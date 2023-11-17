1892c56d0a170132e538f3452c40eb123f536af7
Timothée DURAND <53626355+timothee-durand@users.noreply.github.com>
2023-11-17 18:06:17 +0100

    fix: playground build (#28)

--- src/cadence/src/LoadedLoop.ts
+++ src/cadence/src/LoadedLoop.ts
@@ -1,12 +1,8 @@
-import {Effect, Loop, Time} from './types'
+import { Loop, Time} from './types'
 import {convertTimeToMs} from './utils/convertTimeToMs'
 
-export class LoadedLoop implements Loop {
-	private endTime?: Time
-	private interval: Time
-	private loopEffect?: Effect | Effect[]
-	private sample: string
-	private sampleEffect?: Effect | Effect[]
+export class LoadedLoop {
+	sample: string
 	private speed = 1
 	private startTime: Time = '0ms'
 	private volume = 1
@@ -15,13 +11,9 @@ export class LoadedLoop implements Loop {
 	constructor(buffer: AudioBuffer, loop: Loop) {
 		this.buffer = buffer
 		this.sample = loop.sample
-		this.interval = loop.interval
-		this.sampleEffect = loop.sampleEffect
-		this.loopEffect = loop.loopEffect
 		if (loop.speed) this.speed = loop.speed
 		if (loop.volume) this.volume = loop.volume
 		if (loop.startTime) this.startTime = loop.startTime
-		this.endTime = loop.endTime
 	}
 
 	public loop(): void {
@@ -40,12 +32,6 @@ export class LoadedLoop implements Loop {
 	private get startTimeMs(): number {
 		const startTime = convertTimeToMs(this.startTime)
 		return startTime
-	}
-
-	private get endTimeMs(): number | undefined {
-		if(!this.endTime) return undefined
-		const endTime = convertTimeToMs(this.endTime)
-		return endTime
 	}
 
 	get samplePath(): string {

d3f3e3bf9e55281f389e42e24ce8020d1976d6a2
Timothée DURAND <timothee.durand70@gmail.com>
2023-11-17 18:08:51 +0100

    fix: builded path

--- src/playground/vite.config.ts
+++ src/playground/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
  
 export default defineConfig({
+  base: process.env.APP_ENV === "production" ? '/cadence' : '/',
   plugins: [react()],
   resolve: {
     alias: {

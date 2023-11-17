c16e7224a893d64d197fbddbca5a09e5cb5457c1
mouzone9 <74732988+mouzone9@users.noreply.github.com>
2023-11-17 16:29:19 +0100

    add monaco (#20)
    * add monaco
    
    * add loc files
    
    * fix: lockfile
    
    ---------
    
    Co-authored-by: Timothée DURAND <timothee.durand70@gmail.com>

--- src/playground/src/App.tsx
+++ src/playground/src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
-import {Test} from "./Test.tsx";
+import './index.css'
 import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import React ,{ useRef } from 'react';
+import Editor from '@monaco-editor/react';
+import { ScrollArea } from '@radix-ui/react-scroll-area';
 
 function App() {
+  function test() {
+    console.log("hello world")
+  }
+
   return (
     <>
-      <Test/>
-      <Button>Click me</Button>
+      <div className="bg-slate-800">
+        <div className="flex justify-center">
+          <Button variant="outline">Play</Button>
+          <Button variant="outline">Stop</Button>
+        </div>
+      </div>
+      <Editor 
+        width="500px"
+        height="500px" 
+        theme="vs-dark" 
+        defaultLanguage="javascript"
+        onMount={test}
+        defaultValue="// some code" 
+      />;
     </>
   )
 }
--- src/playground/tailwind.config.js
+++ src/playground/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    "./src/**/*.{html,js}",
 	],
   theme: {
     container: {
